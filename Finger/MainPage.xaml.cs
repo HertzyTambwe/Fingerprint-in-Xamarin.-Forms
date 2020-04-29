b518d6ca3417be541d10bd291a984fbc8c51920b
Ana5Famous <hertytambwe@gmail.com>
2020-04-29 12:09:22 +0200

    Add project files.

---
+++ Finger/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Fingerprint;
+using Plugin.Fingerprint.Abstractions;
+using Xamarin.Forms;
+
+namespace Finger
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private Model model = new Model();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var request = new AuthenticationRequestConfiguration("Prove you have fingers!", "Because without it you can't have access");
+            var result = await CrossFingerprint.Current.AuthenticateAsync(request);
+
+            if (result.Authenticated)
+            {
+                model.Result = "Authenticated via fingerprint";
+            }
+            else
+            {
+                model.Result = "Authentication failed";
+            }
+        }
+    }
+}
---
+++ Finger/Model.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace Finger
+{
+    public class Model : INotifyPropertyChanged
+    {
+        private string result;
+
+        public string Result
+        {
+            get { return result; }
+            set
+            {
+                result = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Result"));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
